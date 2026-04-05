6e32f45ab40301669810e8a21b38eddc1fe09b22
carrm <carrmdev@fastmail.com>
2026-04-05 16:00:36 +0200

    feat: improve demo app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,40 @@
-import { Component, signal } from "@angular/core";
-import { PinyinVirtualKeyboard } from "pinyin-virtual-keyboard";
-import { FormsModule } from "@angular/forms";
+import { Component, computed, signal } from '@angular/core';
+import { PinyinVirtualKeyboard } from 'pinyin-virtual-keyboard';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: "app-root",
+  selector: 'app-root',
   imports: [FormsModule, PinyinVirtualKeyboard],
-  templateUrl: "./app.html",
-  styleUrl: "./app.css",
+  templateUrl: './app.html',
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly mobileValue = signal("");
-  protected readonly landscapeValue = signal("");
-  protected readonly portraitValue = signal("");
+  protected readonly anchor = signal<'container' | 'fixed' | 'custom'>('container');
+  protected readonly format = signal('mobile');
+  protected readonly letterLayout = signal('QWERTY');
+  protected readonly symbolLayout = signal('default');
+  protected readonly numericRow = signal(true);
+  protected readonly simplify = signal(true);
+  protected readonly value = signal('');
+
+  protected readonly pvkLayout = computed(() => {
+    if (this.letterLayout() === 'custom') {
+      return [
+        ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
+        ['K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S'],
+        ['SHIFT', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'BACKSPACE'],
+      ];
+    }
+    return this.letterLayout() as 'QWERTY' | 'AZERTY' | 'QWERTZ';
+  });
+
+  protected readonly pvkSymbolLayout = computed(() => {
+    if (this.symbolLayout() === 'custom') {
+      return [
+        ['.', ',', '_', '"', "'", ':', ';', '!', '?'],
+        ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
+      ];
+    }
+    return 'SYMBOL';
+  });
 }
